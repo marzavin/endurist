deef03635ae37ad36527110d08b305fbc9921c52
Artsiom Marzavin <marzavin@gmail.com>
2025-11-16 23:50:49 +0100

    List pages refactoring

--- frontend/src/components/FilePreviewPanel.tsx
+++ frontend/src/components/FilePreviewPanel.tsx
@@ -14,44 +14,42 @@ function FilePreviewPanel({ file }: Props) {
   }
 
   return (
-    <div className="app-file-preview-panel col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">
-      <div>
-        <div className="app-panel-line row">
-          <div className="col-10 d-flex justify-content-start">
-            <strong>{file.name}</strong>
-          </div>
-          <div className="col-2 d-flex justify-content-end">
-            <span className="app-badge badge app-info-label rounded-pill">
-              {file.extension}
-            </span>
-          </div>
+    <div className="app-card">
+      <div className="app-card-row">
+        <div>
+          <strong>{file.name}</strong>
         </div>
-        <div className="app-panel-line app-font-s row">
-          <div className="col-6 d-flex justify-content-start">
-            <strong>Status:</strong>
-          </div>
-          <div className="col-6 d-flex justify-content-end">
-            {FileStatus[file.status]}
-          </div>
+        <div>
+          <span className="app-badge badge app-info-label rounded-pill">
+            {file.extension}
+          </span>
         </div>
-        <div className="app-panel-line app-font-s row">
-          <div className="col-6 d-flex justify-content-start">
-            <strong>Size:</strong>
-          </div>
-          <div className="col-6 d-flex justify-content-end">
-            {(file.size / 1024).toFixed(2)} KB
-          </div>
+      </div>
+      <div className="app-card-row app-font-s">
+        <div>
+          <strong>Status:</strong>
         </div>
-        <div className="app-panel-line row">
-          <div className="col-12 d-flex justify-content-end">
-            <a
-              className="nav-link app-download-link"
-              title="Download source file"
-              onClick={handleFileDownload}
-            >
-              <i className="bi bi-download" />
-            </a>
-          </div>
+        <div>
+          <span>{FileStatus[file.status]}</span>
+        </div>
+      </div>
+      <div className="app-card-row app-font-s">
+        <div>
+          <strong>Size:</strong>
+        </div>
+        <div>
+          <span>{(file.size / 1024).toFixed(2)} KB</span>
+        </div>
+      </div>
+      <div className="app-card-row">
+        <div>
+          <a
+            className="app-card-link"
+            title="Download source file"
+            onClick={handleFileDownload}
+          >
+            <i className="bi bi-download" />
+          </a>
         </div>
       </div>
     </div>
--- frontend/src/components/ProfilePreviewPanel.tsx
+++ frontend/src/components/ProfilePreviewPanel.tsx
@@ -8,30 +8,28 @@ interface Props {
 
 function ProfilePreviewPanel({ profile }: Props) {
   return (
-    <div className="app-profile-preview-panel col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">
-      <div>
-        <div className="app-panel-line row">
-          <div className="col-12 d-flex justify-content-start">
-            <a href={`/profiles/${profile.id}`}>
-              <strong>{profile.name}</strong>
-            </a>
-          </div>
+    <div className="app-card">
+      <div className="app-card-row">
+        <div>
+          <a href={`/profiles/${profile.id}`}>
+            <strong>{profile.name}</strong>
+          </a>
         </div>
-        <div className="app-panel-line app-font-s row">
-          <div className="col-6 d-flex justify-content-start">
-            <strong>Distance:</strong>
-          </div>
-          <div className="col-6 d-flex justify-content-end">
-            <span>{toKilometersLabel(profile.distance)}</span>
-          </div>
+      </div>
+      <div className="app-card-row app-font-s">
+        <div>
+          <strong>Distance:</strong>
         </div>
-        <div className="app-panel-line app-font-s row">
-          <div className="col-6 d-flex justify-content-start">
-            <strong>Duration:</strong>
-          </div>
-          <div className="col-6 d-flex justify-content-end">
-            <span>{toTimeSpanLabel(profile.duration)}</span>
-          </div>
+        <div>
+          <span>{toKilometersLabel(profile.distance)}</span>
+        </div>
+      </div>
+      <div className="app-card-row app-font-s">
+        <div>
+          <strong>Duration:</strong>
+        </div>
+        <div>
+          <span>{toTimeSpanLabel(profile.duration)}</span>
         </div>
       </div>
     </div>
